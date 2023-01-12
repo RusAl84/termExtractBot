e47cb7726d40f626d3daab7628bb502de4e625f7
Aleksey <rusal@bk.ru>
2023-01-12 19:49:18 +0300

    add bot

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
-﻿
+﻿import telebot
+import os
+import termExtract as te
+
+# Создаем бота, пишем свой токен
+# bot = telebot.TeleBot('Здесь твой токен, полученный от @botfather')
+bot = telebot.TeleBot('5982175377:AAH4V5oGT1kmyCKnrLo--w9RHvEJLW7DI9s')
+
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(
+        m.chat.id, 'Я на связи. Напиши мне текст из которого нужно извлечь ключевые слова')
+
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    s = te.term_extract(message.text)
+    bot.send_message(message.chat.id, s)
+
+
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
--- termExtract.py
+++ termExtract.py
@@ -1,15 +1,20 @@
 ﻿from rutermextract import TermExtractor
 from typing import List
 
-def analyz(text: str) -> List[str]:
+def term_extract(text: str) -> List[str]:
     term_extractor = TermExtractor()
     definition_list: List[str] = list()
     for term in term_extractor.__call__(text, nested=True):
         definition_list.append(term.normalized)
         #print(str(term.normalized.split(' ')))
-    return definition_list
+    str1=""
+    for item in definition_list:
+        if len(item)>16:
+            str1 = str1 + item + "\n "
+        
+    return str1
 
 if __name__ == '__main__':
     data="Системы на основе машинного обучения активно внедряются в самых разных сферах жизни. Но знаете ли вы, как часто они ошибаются и насколько легко их взломать? И можно ли вообще позволять им принимать решения без человеческого контроля?"
-    res= analyz(data)
+    res= term_extract(data)
     print(res)
